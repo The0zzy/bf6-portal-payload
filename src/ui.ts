1bb9ca6319000098baa8b79c75f9215c8e302942
andy6170 <andy6170@hotmail.com>
2026-02-03 21:22:22 +0000

    Creating Branch

--- src/ui.ts
+++ src/ui.ts
@@ -6,8 +6,8 @@ export function initCheckpointTimer(initialRemaining: number): void {
 
     mod.AddUIText(
         "checkpoint_timer",
-        mod.CreateVector(0, 50, 0),
-        mod.CreateVector(400, 50, 0),
+        mod.CreateVector(0, 100, 0),
+        mod.CreateVector(300, 50, 0),
         mod.UIAnchor.TopCenter,
         mod.Message(
             timerKey,
@@ -16,6 +16,15 @@ export function initCheckpointTimer(initialRemaining: number): void {
             mod.Modulo(secs, 10)
         )
     );
+
+    const timer = mod.FindUIWidgetWithName("checkpoint_timer");
+
+    mod.SetUIWidgetBgColor(timer, mod.CreateVector(0, 0, 0));
+    mod.SetUIWidgetBgAlpha(timer, 0.8);
+    mod.SetUITextSize(timer, 26);
+    mod.SetUITextColor(timer, mod.CreateVector(1, 1, 1));
+    mod.SetUITextAlpha(timer, 1);
+    mod.SetUITextAnchor(timer, mod.UIAnchor.Center);
 }
 
 export function updateCheckpointTimer(remainingTime: number): void {
@@ -36,6 +45,7 @@ export function updateCheckpointTimer(remainingTime: number): void {
         );
     }
 }
+
 let friendlycolour = mod.CreateVector(0, 0.8, 1);
 let enemycolour = mod.CreateVector(1, 0.2, 0.2);
 let friendlybgcolour = mod.CreateVector(0, 0.2, 0.5);
@@ -45,32 +55,14 @@ let enemybgcolour = mod.CreateVector(0.6, 0.1, 0.1);
 
 
 export function uiSetup(): void {
-    mod.AddUIContainer("container", mod.CreateVector(0, 50, 0), mod.CreateVector(2000, 2000, 0), mod.UIAnchor.Center);
-
-    // Retrieve the container widget
+    // Container setup
+    mod.AddUIContainer("container", mod.CreateVector(0, 50, 0), mod.CreateVector(2000, 2000, 0), mod.UIAnchor.TopCenter);
     const containerWidget = mod.FindUIWidgetWithName("container");
-    //mod.SetUIWidgetBgFill(containerWidget, mod.UIBgFill.None);
+    mod.SetUIWidgetBgFill(containerWidget, mod.UIBgFill.None);
     mod.SetUIWidgetDepth(containerWidget, mod.UIDepth.AboveGameUI);
 
-    if (containerWidget) {
-        // Add PayloadStatusText
-        // 50 down (Y=50), 85 wide, 30 tall
-        mod.AddUIText(
-            "PayloadStatusText",
-            mod.CreateVector(0, 0, 0),
-            mod.CreateVector(85, 30, 0),
-            mod.UIAnchor.TopCenter,
-            containerWidget,
-            true, // visible
-            0, // padding
-            mod.CreateVector(0, 0, 0), // bgColor
-            0.8, // bgAlpha
-            mod.UIBgFill.Blur, // bgFill
-            mod.Message(mod.stringkeys.payload.state.message, mod.stringkeys.payload.state.idle),
-            20, // textSize
-            mod.CreateVector(1, 1, 1), // textColor
-            1, // textAlpha
-            mod.UIAnchor.Center
-        );
-    }
+    // Payload status
+    mod.AddUIText("payloadstatus", mod.CreateVector(0, 50, 0), mod.CreateVector(200, 30, 0), mod.UIAnchor.TopCenter, containerWidget, true, 0, mod.CreateVector(0, 0, 0), 0.8, mod.UIBgFill.None, mod.Message(mod.stringkeys.payload.state.message, mod.stringkeys.payload.state.idle), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center);
+    mod.AddUIContainer("progressbackground", mod.CreateVector(0, 0, 0), mod.CreateVector(600, 5, 0), mod.UIAnchor.TopCenter, containerWidget, true, 0, mod.CreateVector(0.6, 0.1, 0.1), 0.8, mod.UIBgFill.Solid);
+    mod.AddUIContainer("progress", mod.CreateVector(0, 0, 0), mod.CreateVector(10, 5, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(0, 0.6, 1), 1, mod.UIBgFill.Solid);
 }
