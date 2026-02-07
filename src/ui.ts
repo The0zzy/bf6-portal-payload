2e418198d1a5f4e0474524752dd97c2067cd95a0
andy6170 <andy6170@hotmail.com>
2026-02-07 20:34:20 +0000

    UI Progress Bar and Percentage Complete

--- src/ui.ts
+++ src/ui.ts
@@ -37,7 +37,7 @@ export function updateCheckpointTimer(remainingTime: number): void {
     mod.SetUITextLabel(mod.FindUIWidgetWithName("remaining_time2"), mod.Message(timerKey, mins, mod.Floor(secs / 10), mod.Modulo(secs, 10)));
 }
 
-let friendlycolour = mod.CreateVector(0, 0.4, 1); //0, 0.8, 1
+let friendlycolour = mod.CreateVector(0, 0.8, 1); //0, 0.8, 1
 let enemycolour = mod.CreateVector(1, 0.2, 0.2);
 let friendlybgcolour = mod.CreateVector(0, 0.15, 0.3);
 let enemybgcolour = mod.CreateVector(0.4, 0, 0);
@@ -54,22 +54,32 @@ export function uiSetup(): void {
 
     // Payload status
     mod.AddUIText("payloadstatus", mod.CreateVector(0, 30, 0), mod.CreateVector(200, 30, 0), mod.UIAnchor.TopCenter, containerWidget, true, 0, mod.CreateVector(0, 0, 0), 0.9, mod.UIBgFill.None, mod.Message(mod.stringkeys.payload.state.message, mod.stringkeys.payload.state.idle), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center);
-    mod.AddUIContainer("progress_background1", mod.CreateVector(0, 5, 0), mod.CreateVector(600, 10, 0), mod.UIAnchor.TopCenter, containerWidget, true, 0, enemybgcolour, 0.9, mod.UIBgFill.Solid, mod.GetTeam(1));
-    mod.AddUIContainer("progress1", mod.CreateVector(150, 0, 0), mod.CreateVector(6 * STATE.progressInPercent, 20, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, friendlycolour, 1, mod.UIBgFill.Solid, mod.GetTeam(1));
-    mod.AddUIContainer("progress_background2", mod.CreateVector(0, 5, 0), mod.CreateVector(600, 10, 0), mod.UIAnchor.TopCenter, containerWidget, true, 0, friendlybgcolour, 0.9, mod.UIBgFill.Solid, mod.GetTeam(2));
-    mod.AddUIContainer("progress2", mod.CreateVector(150, 0, 0), mod.CreateVector(6 * STATE.progressInPercent, 20, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, enemycolour, 1, mod.UIBgFill.Solid, mod.GetTeam(2));
-    mod.AddUIContainer("checkpoint1", mod.CreateVector(146, -5, 0), mod.CreateVector(4, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(1, 1, 1), 1, mod.UIBgFill.Solid);
-    mod.AddUIContainer("checkpoint2", mod.CreateVector(750, -5, 0), mod.CreateVector(4, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(1, 1, 1), 1, mod.UIBgFill.Solid);
-    mod.AddUIContainer("checkpoint3", mod.CreateVector(300, -5, 0), mod.CreateVector(4, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(1, 1, 1), 1, mod.UIBgFill.Solid);
+    mod.AddUIContainer("progress_background1", mod.CreateVector(150, 5, 0), mod.CreateVector(600 - (6 * STATE.progressInPercent), 10, 0), mod.UIAnchor.TopRight, containerWidget, true, 0, enemycolour, 0.7, mod.UIBgFill.Solid, mod.GetTeam(1));
+    mod.AddUIContainer("progress1", mod.CreateVector(150, 0, 0), mod.CreateVector(6 * STATE.progressInPercent, 20, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, friendlycolour, 0.7, mod.UIBgFill.Solid, mod.GetTeam(1));
+    mod.AddUIContainer("progress_background2", mod.CreateVector(150, 5, 0), mod.CreateVector(600 - (6 * STATE.progressInPercent), 10, 0), mod.UIAnchor.TopRight, containerWidget, true, 0, friendlycolour, 0.7, mod.UIBgFill.Solid, mod.GetTeam(2));
+    mod.AddUIContainer("progress2", mod.CreateVector(150, 0, 0), mod.CreateVector(6 * STATE.progressInPercent, 20, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, enemycolour, 0.7, mod.UIBgFill.Solid, mod.GetTeam(2));
+    mod.AddUIContainer("checkpoint0", mod.CreateVector(146, -5, 0), mod.CreateVector(4, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(1, 1, 1), 1, mod.UIBgFill.Solid);
     mod.AddUIText("remaining_time1", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopRight, containerWidget, true, 0, enemybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.idle), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(1));
     mod.AddUIText("remaining_time2", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopRight, containerWidget, true, 0, friendlybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.idle), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(2));
-    mod.AddUIText("percentage1", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, friendlybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.percentage, mod.RoundToInteger(STATE.progressInPercent)), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(1));
-    mod.AddUIText("percentage2", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, enemybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.percentage, mod.RoundToInteger(STATE.progressInPercent)), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(2));
+    mod.AddUIText("percentage1", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, friendlybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.percentage, mod.Floor(STATE.progressInPercent)), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(1));
+    mod.AddUIText("percentage2", mod.CreateVector(0, -5, 0), mod.CreateVector(100, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, enemybgcolour, 0.9, mod.UIBgFill.Solid, mod.Message(mod.stringkeys.payload.state.percentage, mod.Floor(STATE.progressInPercent)), 26, mod.CreateVector(1, 1, 1), 1, mod.UIAnchor.Center, mod.GetTeam(2));
+
+    //Checkpoints distance on progress UI
+    for (let i = 1; i < STATE.waypoints.size; i++) {
+        if (STATE.waypoints.get(i)!.isCheckpoint) {
+            mod.AddUIContainer("checkpoint" + i,
+                mod.CreateVector(146 + (6 * ((STATE.waypoints.get(i)!.distance / STATE.totalDistanceInMeters) * 100)), -5, 0),
+                mod.CreateVector(4, 30, 0), mod.UIAnchor.TopLeft, containerWidget, true, 0, mod.CreateVector(1, 1, 1), 1, mod.UIBgFill.Solid);
+        }
+    }
+
 }
 
 export function updateUI(): void {
     mod.SetUIWidgetSize(mod.FindUIWidgetWithName("progress1"), mod.CreateVector(6 * STATE.progressInPercent, 20, 0));
     mod.SetUIWidgetSize(mod.FindUIWidgetWithName("progress2"), mod.CreateVector(6 * STATE.progressInPercent, 20, 0));
-    mod.SetUITextLabel(mod.FindUIWidgetWithName("percentage1"), mod.Message(mod.stringkeys.payload.state.percentage, mod.RoundToInteger(STATE.progressInPercent)));
-    mod.SetUITextLabel(mod.FindUIWidgetWithName("percentage2"), mod.Message(mod.stringkeys.payload.state.percentage, mod.RoundToInteger(STATE.progressInPercent)));
+    mod.SetUITextLabel(mod.FindUIWidgetWithName("percentage1"), mod.Message(mod.stringkeys.payload.state.percentage, mod.Floor(STATE.progressInPercent)));
+    mod.SetUITextLabel(mod.FindUIWidgetWithName("percentage2"), mod.Message(mod.stringkeys.payload.state.percentage, mod.Floor(STATE.progressInPercent)));
+    mod.SetUIWidgetSize(mod.FindUIWidgetWithName("progress_background1"), mod.CreateVector(600 - (6 * STATE.progressInPercent), 10, 0));
+    mod.SetUIWidgetSize(mod.FindUIWidgetWithName("progress_background2"), mod.CreateVector(600 - (6 * STATE.progressInPercent), 10, 0));
 }
