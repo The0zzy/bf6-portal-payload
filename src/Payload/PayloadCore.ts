9235d689a97153dc03e3e51f92ba75014a44edc9
Eric Wünsche <wuensche.e@gmail.com>
2026-04-17 15:28:41 +0200

    Revert "add capture point for checkpoints"
    This reverts commit 513f4f35a6423aa4b9eac36a9427ffac6bba8c57.

--- src/Payload/PayloadCore.ts
+++ src/Payload/PayloadCore.ts
@@ -361,6 +361,9 @@ export class PayloadCore {
         const reachedCheckpointWpIndex = PayloadState.instance.checkpointIndexes[
             PayloadState.instance.reachedCheckpointIndex
         ];
+        const nextCheckpointWpIndex = PayloadState.instance.checkpointIndexes[
+            PayloadState.instance.reachedCheckpointIndex + 1
+        ];
         for (const checkpointWpIndex of PayloadState.instance.checkpointIndexes) {
             const waypoint = PayloadState.instance.waypoints[checkpointWpIndex];
             for (let s = 0; s < PayloadConfig.checkpointSpatials.length; s++) {
@@ -388,18 +391,21 @@ export class PayloadCore {
                     PayloadState.instance.checkpointObjectives.delete(key);
                 }
 
-                const objectiveConfig = PayloadConfig.checkpointObjectives[o];
-                const spawnPos = mod.Add(waypoint.position, objectiveConfig.relativeOffset);
-                const spawnRot = mod.Add(waypoint.rotation, objectiveConfig.rotation);
-                const obj = mod.SpawnObject(
-                    objectiveConfig.prefab,
-                    spawnPos,
-                    spawnRot,
-                    objectiveConfig.scale
-                ) as mod.CapturePoint;
-                const owner: mod.Team = checkpointWpIndex <= reachedCheckpointWpIndex ? mod.GetTeam(1) : mod.GetTeam(2);
-                mod.SetCapturePointOwner(obj, owner);
-                PayloadState.instance.checkpointObjectives.set(key, obj);
+                // only spawn checkpoint objectives for the next upcoming checkpoint
+                if (
+                    checkpointWpIndex === nextCheckpointWpIndex
+                ) {
+                    const objectiveConfig = PayloadConfig.checkpointObjectives[o];
+                    const spawnPos = mod.Add(waypoint.position, objectiveConfig.relativeOffset);
+                    const spawnRot = mod.Add(waypoint.rotation, objectiveConfig.rotation);
+                    const obj = mod.SpawnObject(
+                        objectiveConfig.prefab,
+                        spawnPos,
+                        spawnRot,
+                        objectiveConfig.scale
+                    );
+                    PayloadState.instance.checkpointObjectives.set(key, obj as mod.CapturePoint);
+                }
             }
 
             for (let v = 0; v < PayloadConfig.checkpointVfx.length; v++) {
